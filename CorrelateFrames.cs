c6596dba7868341294673195eda8bb5a2f9ffa90
Joshua Soong <joshua@soong.co>
2020-01-10 00:25:26 +1300

    insane performance improvement
    i am a genius
    
    https://www.codeproject.com/Questions/1039081/Difference-in-Brightness-GetPixel-x-y-GetBrightnes

--- CorrelateFrames.cs
+++ CorrelateFrames.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.CommandLine;
 using System.CommandLine.Invocation;
 using System.IO;
@@ -56,87 +57,93 @@ namespace whiterabbitc
                 Bitmap[] frameArray = new Bitmap[frames];
                 for (int i = 0; i < frames; i++) {frameArray[i] = new Bitmap(fileArray[i].FullName);}
                 CorrelateFrames CF = new CorrelateFrames();
-                float[] correlationData = CF.plotCorrelation(mask, frameArray);
+                double[] correlationData = CF.plotCorrelation(mask, frameArray);
             });
 
             return rootCommand.InvokeAsync(args).Result;
         }
 
-        public float[] plotCorrelation(int maskSize, Bitmap[] frameArray)
+        public double[] plotCorrelation(int maskSize, Bitmap[] frameArray)
         {
             int frameCount = frameArray.Length;
-            float[] correlation = new float[frameCount - 1];
+            double[] correlation = new double[frameCount - 1];
 
             for (int i = 1; i <= frameCount - 1; i++)
             {
-                float[] corrArray = new float[frameCount - i];
+                double corrSum = 0;
 
                 for (int j = 1; j <= frameCount - i; j++)
                 {
-                    corrArray[j - 1] = getR(getMeanFrame(maskSize, frameArray[j - 1]), getMeanFrame(maskSize, frameArray[i + j - 1]));
+                    corrSum += getR(getMeanFrame(maskSize, frameArray[j - 1]), getMeanFrame(maskSize, frameArray[i + j - 1]));
                 }
-                correlation[i - 1] = corrArray.Average();
+                correlation[i - 1] = corrSum / (frameCount - i);
                 Console.WriteLine(correlation[i - 1]);
             }
             return correlation;
         }
 
-        public float[] getMeanFrame(int maskSize, Bitmap frame)
+        public double[] getMeanFrame(int maskSize, Bitmap frame)
         {
-            int masksX = (int) (frame.Width / maskSize);
-            int masksY = (int) (frame.Height / maskSize);
-
-            float[] maskArray = new float[masksX * masksY];
-            float[] localArea = new float[maskSize * maskSize];
-
             int counter = 0;
+            double[] maskArray;
 
-            for (int i = 0; i < masksY; i++)
+            unsafe
             {
-                int yStep = i * maskSize;
-                int yStepMask = yStep + maskSize;
+                BitmapData frameData = frame.LockBits(new Rectangle(0, 0, frame.Width, frame.Height), ImageLockMode.ReadWrite, frame.PixelFormat);
+                int masksX = (int) (frameData.Width / maskSize);
+                int masksY = (int) (frameData.Height / maskSize);
+                double[] maskArrayUnsafe = new double[masksX * masksY];
+                int bytesPerPixel = System.Drawing.Bitmap.GetPixelFormatSize(frame.PixelFormat) / 8;
+                int maskBytes = bytesPerPixel * maskSize;
+                int heightInPixels = frameData.Height;
+                int widthInBytes = frameData.Width * bytesPerPixel;
+                byte* PtrFirstPixel = (byte*)frameData.Scan0;
 
-                for (int j = 0; j < masksX; j++)
+                for (int i = 0; i < masksY; i++)
                 {
-                    int xStep = j * maskSize;
-                    int xStepMask = xStep + maskSize;
-                    int index = 0;
+                    int yStep = i * maskSize;
 
-                    for (int y = yStep; y < yStepMask; y++)
+                    for (int j = 0; j < masksX; j++)
                     {
-                        for (int x = xStep; x < xStepMask; x++)
+                        int xStep = j * maskSize * bytesPerPixel;
+                        double sum = 0;
+
+                        for (int y = yStep; y < yStep + maskSize; y++)
                         {
-                            localArea[index] = frame.GetPixel(x, y).GetBrightness();
-                            index++;
+                            byte* currentLine = PtrFirstPixel + (y * frameData.Stride);
+                            for (int x = xStep; x < xStep + maskBytes; x = x + bytesPerPixel)
+                            {
+                                sum += (double)currentLine[x]/255.00;
+                            }
                         }
+                        maskArrayUnsafe[counter] = sum / (maskSize * maskSize);
+                        counter++;
                     }
-                    maskArray[counter] = localArea.Average();
-                    counter++;
                 }
+                maskArray = maskArrayUnsafe;
+                frame.UnlockBits(frameData);
             }
             return maskArray;
         }
 
-        public float getR(float[] frame1, float[] frame2)
+        public double getR(double[] frame1, double[] frame2)
         {
             var avg1 = frame1.Average();
             var avg2 = frame2.Average();
 
             var sum1 = frame1.Zip(frame2, (x1, y1) => (x1 - avg1) * (y1 - avg2)).Sum();
 
-            var sumSqr1 = (float) (frame1.Sum(x => Math.Pow((x - avg1), 2.0)));
-            var sumSqr2 = (float) (frame2.Sum(y => Math.Pow((y - avg2), 2.0)));
+            var sumSqr1 = frame1.Sum(x => Math.Pow((x - avg1), 2));
+            var sumSqr2 = frame2.Sum(y => Math.Pow((y - avg2), 2));
 
-            var result = (float) (sum1 / Math.Sqrt(sumSqr1 * sumSqr2));
-
-            return result;
+            return sum1 / Math.Sqrt(sumSqr1 * sumSqr2);
         }
 
-        public float getStdDev(float mean, float[] frame)
+        public double getStdDev(double mean, double[] frame)
         {
-            float stdDev = 0;
-			foreach (float fr in frame) {stdDev += (mean - fr) * (mean - fr);}
-			return (float) (Math.Sqrt(stdDev / (frame.Length - 1)));
+            double stdDev = 0;
+			foreach (double mask in frame) {stdDev += Math.Pow((mean - mask), 2);}
+			return Math.Sqrt(stdDev / (frame.Length - 1));
         }
     }
 }
